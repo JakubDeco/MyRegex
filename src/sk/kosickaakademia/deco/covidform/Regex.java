3ab84d41ba451228d74d2bba22acbf1ced4623e8
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-01-20 14:53:52 +0100

    added Regex.isSPZValid with Unit5test

--- src/sk/kosickaakademia/deco/covidform/Regex.java
+++ src/sk/kosickaakademia/deco/covidform/Regex.java
@@ -1,5 +1,11 @@
 package sk.kosickaakademia.deco.covidform;
 
-public class Regex {
+import java.util.regex.Pattern;
 
+public class Regex {
+    public boolean isSPZValid(String str){
+        if (str==null) return false;
+        str=str.toUpperCase().trim();
+        return Pattern.matches("[A-Z]{2}\\w{5}",str);
+    }
 }
---
+++ test/sk/kosickaakademia/deco/covidform/RegexTest.java
@@ -0,0 +1,34 @@
+package sk.kosickaakademia.deco.covidform;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegexTest {
+    private Regex object;
+
+    @BeforeEach
+    void setUp() {
+        object=new Regex();
+    }
+
+    @Test
+    void isSPZValid() {
+        assertTrue(object.isSPZValid("ab1M3Ka"));
+        assertTrue(object.isSPZValid("ab123ka  "));
+        assertTrue(object.isSPZValid(" ab12345  "));
+        assertTrue(object.isSPZValid(" ABeztox  "));
+        assertTrue(object.isSPZValid(" abezto9  "));
+        assertTrue(object.isSPZValid("ab1zto9  "));
+        assertTrue(object.isSPZValid("ab1zto9"));
+
+        assertFalse(object.isSPZValid("1b1zto9"));
+        assertFalse(object.isSPZValid(""));
+        assertFalse(object.isSPZValid("1"));
+        assertFalse(object.isSPZValid("A"));
+        assertFalse(object.isSPZValid("ak123o"));
+        assertFalse(object.isSPZValid("a9123oz"));
+        assertFalse(object.isSPZValid(null));
+    }
+}
