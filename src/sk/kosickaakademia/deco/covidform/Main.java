a36bfb8f4ab7269af887ecbf92dc8ff30d90c5f7
JakubDeco <jakub.deco@kosickaakademia.sk>
2021-01-22 09:47:31 +0100

    added Tag.checkFileValues, added resource/money.txt

--- src/sk/kosickaakademia/deco/covidform/Main.java
+++ src/sk/kosickaakademia/deco/covidform/Main.java
@@ -1,7 +1,10 @@
 package sk.kosickaakademia.deco.covidform;
 
+import sk.kosickaakademia.deco.pricetag.Tag;
+
 public class Main {
     public static void main(String[] args) {
-
+        Tag tag1=new Tag();
+        tag1.checkFileValues("resource/money.txt");
     }
 }
--- src/sk/kosickaakademia/deco/pricetag/Tag.java
+++ src/sk/kosickaakademia/deco/pricetag/Tag.java
@@ -1,5 +1,8 @@
 package sk.kosickaakademia.deco.pricetag;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class Tag {
@@ -7,5 +10,23 @@ public class Tag {
         if (str==null) return false;
         str=str.trim();
         return Pattern.matches("^(0|0(,|.)(\\d[1-9]|[1-9]\\d)|[1-9]\\d*((,|.)(\\d[1-9]|[1-9]\\d)?))$",str);
+    }
+
+    public void checkFileValues(String filepath){
+        try{
+            FileReader fileReader=new FileReader(filepath);
+            BufferedReader bufferedReader=new BufferedReader(fileReader);
+
+            String line;
+            while ((line=bufferedReader.readLine())!=null){
+                System.out.print(line+" - ");
+                if (isPriceTagValid(line))
+                    System.out.println("ano");
+                else System.out.println("nie");
+            }
+            fileReader.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
